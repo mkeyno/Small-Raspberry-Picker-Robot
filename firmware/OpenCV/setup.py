38d563a24ddafc55c6a1a8c992a10118f9d6f035
Mehrdad K <lkeyno@yahoo.com>
2019-01-29 21:23:50 +1300

    Update setup.py

--- firmware/OpenCV/setup.py
+++ firmware/OpenCV/setup.py
@@ -1,1 +1,22 @@
 
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="FruitDetector",
+    version="0.0.1-edf01",
+    author="Mehrdad Keyno",
+    author_email="hrsk1980@gmail.com",
+    description="A primilarly fuction to detect berries on the camera",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mkeyno/Small-Raspberry-Picker-Robot/blob/master/firmware/OpenCV",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
