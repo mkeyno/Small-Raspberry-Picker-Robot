d589505db4101411a2ee2c5f6cf65bfd388ad101
Mehrdad K <lkeyno@yahoo.com>
2019-07-28 15:46:58 +1200

    Create winApp.py

---
+++ firmware/OpenCV/fruitDetector/winApp.py
@@ -0,0 +1,12 @@
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tkinter as win
+
+def config():
+  pass
+  
+def run():
+  windows=win.Tk()
+  windows.title("fjghj")
+  windows.mainloop()
