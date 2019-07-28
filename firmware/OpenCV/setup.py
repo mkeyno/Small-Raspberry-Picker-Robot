2de9af785ea3e8b75db07d921c566bb01b61119f
Mehrdad K <lkeyno@yahoo.com>
2019-07-28 16:41:35 +1200

    Update setup.py

--- firmware/OpenCV/setup.py
+++ firmware/OpenCV/setup.py
@@ -1,11 +1,18 @@
 
-import setuptools
+import pathlib
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+long_description = (HERE / "README.md").read_text()
+
+#with open("README.md", "r") as fh:
+#    long_description = fh.read()
 
 setuptools.setup(
-    name="FruitDetector",
+    name="KeynoRobot",
     version="0.0.1-edf01",
     author="Mehrdad Keyno",
     author_email="hrsk1980@gmail.com",
@@ -13,11 +20,16 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mkeyno/Small-Raspberry-Picker-Robot/blob/master/firmware/OpenCV",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=("tests",)),
     install_requires=[
           'numpy',
           'cv2',
       ],
+    
+#   entry_points={
+#        "console_scripts": [
+#            "realpython=reader.__main__:main",
+#        ]
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
