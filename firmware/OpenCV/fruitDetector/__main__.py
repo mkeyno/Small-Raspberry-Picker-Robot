72ef81bcfdc41c17ea74d4421dd63c2bf74748c1
Mehrdad K <lkeyno@yahoo.com>
2019-07-28 15:36:05 +1200

    Update __main__.py

--- firmware/OpenCV/fruitDetector/__main__.py
+++ firmware/OpenCV/fruitDetector/__main__.py
@@ -1,1 +1,26 @@
 
+# __main__.py
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tkinter as win
+import cv2
+import numpy as np
+
+import tensorflow as tf
+import sys
+import platform
+
+version=.0.1
+
+
+
+def main():
+    """Read the Real Python article feed"""
+    print(sys.argv) 
+    windows=win.Tk()
+    windows.title(version)
+    windows.mainloop()
+
+if __name__ == "__main__":
+    main()
